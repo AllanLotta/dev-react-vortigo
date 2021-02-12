e864351b4bdaa3b4fa31224e93ab59dc7c650174
Allan Lotta <allan.lopes@multivarejogpa.com.br>
2021-02-12 19:04:17 -0300

    fixed styles

--- src/screens/SignUp/styles.ts
+++ src/screens/SignUp/styles.ts
@@ -7,6 +7,7 @@ export const Container = styled.SafeAreaView`
 	flex: 1;
 	align-items: center;
 `;
+
 export const Content = styled.View`
 	flex: 1;
 	width: ${width * 0.9}px;
@@ -21,6 +22,7 @@ export const Title = styled.Text`
 	color: ${colors.LightText};
 	margin-top: ${height * 0.1}px;
 `;
+
 export const Description = styled.Text`
 	font-size: ${RFValue(20)}px;
 	color: ${colors.LightText};
@@ -37,3 +39,34 @@ export const NextButton = styled.TouchableOpacity`
 	bottom: 50px;
 `;
 export const NextButtonImage = styled.Image``;
+
+export const Card = styled.TouchableOpacity`
+	flex-direction: row;
+	align-items: center;
+	justify-content: flex-start;
+	padding: 20px;
+`;
+
+export const CardAvatar = styled.Image`
+	width: 60px;
+	height: 60px;
+`;
+
+export const CardName = styled.Text`
+	flex: 0.5;
+	font-size: ${RFValue(22)}px;
+	font-weight: 500;
+	margin-left: 10px;
+	text-transform: capitalize;
+`;
+
+export const CardRadio = styled.Image`
+	position: absolute;
+	right: 20px;
+`;
+
+export const ConfirmButtonContainer = styled.View`
+	padding-top: 20px;
+	padding-bottom: 50px;
+	align-items: center;
+`;
