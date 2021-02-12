b46610ecfe549d25f943537c666868abefab85d6
Allan Lotta <allan.lopes@multivarejogpa.com.br>
2021-02-12 19:05:06 -0300

    added header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,51 @@
+import { PokemonType } from '@/interfaces/index.js';
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { DB } from '@/db';
+
+import {
+	Container,
+	Content,
+	Header,
+	HeaderTitle,
+	HeaderSearchButton,
+	CardType,
+	CardTypeImage,
+	CardTypeName,
+	List,
+	CardItem,
+	CardItemAvatar,
+	CardItemName,
+} from './styles';
 
 const Home: React.FC = () => {
+	const renderTypeItem = (item: PokemonType) => {
+		return (
+			<CardType key={item.name}>
+				<CardTypeImage source={{ uri: item.thumbnailImage }} />
+				<CardTypeName>{item.name}</CardTypeName>
+			</CardType>
+		);
+	};
 	return (
-		<SafeAreaView style={{ flex: 1 }}>
-			<Text>Home</Text>
-		</SafeAreaView>
+		<Container>
+			<Header>
+				<HeaderTitle>Pokemon Finder</HeaderTitle>
+				<HeaderSearchButton>
+					<Icon name="search" size={25} color="white" />
+				</HeaderSearchButton>
+			</Header>
+			<List
+				horizontal
+				showsHorizontalScrollIndicator={false}
+				keyExtractor={item => item.name}
+				data={DB.types}
+				renderItem={({ item }) => renderTypeItem(item)}
+			/>
+
+			<Content />
+		</Container>
 	);
 };
 
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,57 @@
+import colors from '@/config/theme';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+	flex: 1;
+`;
+
+export const Content = styled.SafeAreaView`
+	flex: 1;
+`;
+
+export const Header = styled.View`
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	background-color: ${colors.Primary};
+	padding: 20px;
+	padding-top: ${getStatusBarHeight() + 20}px;
+`;
+
+export const HeaderTitle = styled.Text`
+	font-size: ${RFValue(22)}px;
+	color: ${colors.LightText};
+`;
+
+export const HeaderSearchButton = styled.TouchableOpacity`
+	position: absolute;
+	right: 20px;
+	bottom: 20px;
+`;
+
+export const CardType = styled.TouchableOpacity`
+	align-items: center;
+	padding: 20px;
+`;
+
+export const CardTypeImage = styled.Image`
+	width: 50px;
+	height: 50px;
+`;
+
+export const CardTypeName = styled.Text`
+	font-size: ${RFValue(22)}px;
+	text-transform: capitalize;
+`;
+
+export const List = styled.FlatList`
+	flex-grow: 0;
+`;
+
+export const CardItem = styled.TouchableOpacity``;
+
+export const CardItemAvatar = styled.Image``;
+
+export const CardItemName = styled.Text``;
